c79398b79495be1c682e11d692688e4f6d9db5f0
S550Stang <prabhuvardhanrao94@gmail.com>
2021-11-01 18:28:04 +0000

    refactored

---
+++ findKey.js
@@ -0,0 +1,45 @@
+const findKey = (obj, callBack) => {
+  console.log(Object.keys(obj));
+  for (let key in obj) {
+    if (callBack(obj[key])) {
+      return key;
+    }
+  }
+};
+
+console.log(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  )
+);
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
